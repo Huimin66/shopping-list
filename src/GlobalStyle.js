950db349b5b4c293aca95e122a8e7471d905ae25
Huimin Cao <huim.cao@gmail.com>
2022-06-13 09:25:17 +0200

    Change list title

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,9 +13,9 @@ const GlobalStyle = createGlobalStyle`
     margin: 1.5rem;
     padding: 0;
     display: grid;
-    grid-template-rows: 60px auto 60px;
+
     height: 100vh;
-    background-color: rgb(132 148 161);
+
     font-family: sans-serif;
     font-size:2rem;
 
